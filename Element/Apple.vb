4341f555216ab4308722783e7bcdbe34363531cf
Apple <bryantconan@yahoo.co.jp>
2020-01-18 17:04:20 +0800

    添加【Apple.vb】文件

---
+++ Element/Apple.vb
@@ -0,0 +1,28 @@
+﻿' 贪吃蛇游戏【苹果】类
+Public Class Apple
+    Private _location As Point  ' 苹果的定位像素
+    Private _body As Block  ' 苹果
+
+    Public Sub New(location As Point, body As Block)
+        _location = location
+        _body = body
+    End Sub
+
+    Public Property Location As Point
+        Get
+            Return _location
+        End Get
+        Set(value As Point)
+            _location = value
+        End Set
+    End Property
+
+    Public Property Body As Block
+        Get
+            Return _body
+        End Get
+        Set(value As Block)
+            _body = value
+        End Set
+    End Property
+End Class
