a613b2926f220527dc8e7ed51455f2301fbd4623
Apple <bryantconan@yahoo.co.jp>
2020-01-19 11:53:12 +0800

    添加【像素设计.xlsx】文件

--- GameForm.vb
+++ GameForm.vb
@@ -6,19 +6,12 @@
     End Sub
 
     Private Sub BTN_Start_Click(sender As Object, e As EventArgs) Handles BTN_Start.Click
-        'Block.InitGraphics(PN_GamePad.CreateGraphics)
-        'console.CreateApple()
-        Dim graphics As Graphics = PN_GamePad.CreateGraphics
-        Dim pen As Pen = New Pen(Color.Red, 0)
-        Dim rec As Rectangle = New Rectangle(0, 0, 1, 1)
-        graphics.DrawRectangle(pen, rec)
-        'graphics.DrawEllipse(pen, 0, 1, 1, 1)
-        pen.Color = Color.LightGray
-        'graphics.DrawEllipse(pen, 0, 2, 1, 1)
-        rec.X = 1
-        graphics.DrawRectangle(pen, rec)
-        rec.X = 0
-        rec.Y = 1
-        graphics.DrawRectangle(pen, rec)
+        Block.InitGraphics(PN_GamePad.CreateGraphics)
+        console.CreateApple()
+        console.CreateSnake()
+    End Sub
+
+    Private Sub GB_Score_Enter(sender As Object, e As EventArgs) Handles GB_Score.Enter
+
     End Sub
 End Class
