3f6deb514abe7184055f30bbd51129e7ae1e9f0d
Apple <bryantconan@yahoo.co.jp>
2020-01-19 17:37:49 +0800

    编写【上，下，左，右】四个移动方向。

--- GameForm.vb
+++ GameForm.vb
@@ -26,27 +26,45 @@
         console.CreateSnake()
         console.CreateApple()
         console.Direction = GameConsole.Direction_Right  ' 初始方向朝右
-        TI_Game.Enabled = True
+        TI_Game.Enabled = True  ' 计时器开始计时
+
+        PN_GamePad.Focus()
+
+        BTN_Start.Enabled = False
+        BTN_Pause.Enabled = True
+        BTN_Stop.Enabled = True
     End Sub
 
     ' Right
     Private Sub BTN_Right_Click(sender As Object, e As EventArgs) Handles BTN_Right.Click
-        LB_Score.Text = "Right"
+        If console.Direction = GameConsole.Direction_Left Then  ' 当前方向朝左时不可调整方向
+            Exit Sub
+        End If
+        console.Direction = GameConsole.Direction_Right  ' 调整方向
     End Sub
 
     ' Left
     Private Sub BTN_Left_Click(sender As Object, e As EventArgs) Handles BTN_Left.Click
-
+        If console.Direction = GameConsole.Direction_Right Then  ' 当前方向朝右时不可调整方向
+            Exit Sub
+        End If
+        console.Direction = GameConsole.Direction_Left  ' 调整方向
     End Sub
 
     ' Up
     Private Sub BTN_Up_Click(sender As Object, e As EventArgs) Handles BTN_Up.Click
-
+        If console.Direction = GameConsole.Direction_Down Then  ' 当前方向朝下时不可调整方向
+            Exit Sub
+        End If
+        console.Direction = GameConsole.Direction_Up  ' 调整方向
     End Sub
 
     ' Down
     Private Sub BTN_Down_Click(sender As Object, e As EventArgs) Handles BTN_Down.Click
-
+        If console.Direction = GameConsole.Direction_Up Then  ' 当前方向朝上时不可调整方向
+            Exit Sub
+        End If
+        console.Direction = GameConsole.Direction_Down  ' 调整方向
     End Sub
 
     ' 结束
@@ -56,6 +74,20 @@
 
     ' 计时器
     Private Sub TI_Game_Tick(sender As Object, e As EventArgs) Handles TI_Game.Tick
+        Select Case console.Direction  ' 判断贪吃蛇当前移动的方向
+            Case GameConsole.Direction_Up  ' Up
+                console.Move(0, -GameConsole.Delta)
+            Case GameConsole.Direction_Down  ' Down
+                console.Move(0, GameConsole.Delta)
+            Case GameConsole.Direction_Left  ' Left
+                console.Move(-GameConsole.Delta, 0)
+            Case GameConsole.Direction_Right  ' Right
+                console.Move(GameConsole.Delta, 0)
+        End Select
+    End Sub
 
+    ' 暂停
+    Private Sub BTN_Pause_Click(sender As Object, e As EventArgs) Handles BTN_Pause.Click
+        TI_Game.Enabled = False  ' 暂停计时器
     End Sub
 End Class
